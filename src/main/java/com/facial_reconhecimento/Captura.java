2e993fe380214cb46c4dc8974ba6ba4842f1081e
Keren Apuque <cardosokerenapuque@gmail.com>
2024-10-25 16:34:05 -0300

    feat: adicionada a captura da tela por meio de interrupção da tecla Q

--- src/main/java/com/facial_reconhecimento/Captura.java
+++ src/main/java/com/facial_reconhecimento/Captura.java
@@ -1,6 +1,7 @@
 package com.facial_reconhecimento;
 
 import java.awt.event.KeyEvent;
+import java.util.Scanner;
 
 import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.opencv.opencv_core.Mat;
@@ -16,10 +17,12 @@ import org.bytedeco.javacv.OpenCVFrameGrabber;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
 import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
 
 public class Captura {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception, InterruptedException {
         KeyEvent teclado = null;
         OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
@@ -30,21 +33,55 @@ public class Captura {
         CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
         Frame frameCapturado = null;
         Mat imagemColorida = new Mat();
+        int numeroAmostras = 25;
+        int countAmostra = 1;
+        System.out.println("Digite seu id: ");
+        Scanner cadastro = new Scanner(System.in);
+        int idPessoa = cadastro.nextInt();
 
         while ((frameCapturado = camera.grab()) != null) {
             imagemColorida = converter.convert(frameCapturado);
             Mat imagemCinza = new Mat();
-            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
             RectVector facesDetectadas = new RectVector();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+
             detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150),
                     new Size(500, 500));
+
+            if (teclado == null) {
+                teclado = cFrame.waitKey(5);
+            }
 
             for (int i = 0; i < facesDetectadas.size(); i++) {
                 Rect dadosFace = facesDetectadas.get(0);
                 rectangle(imagemColorida, dadosFace, new Scalar(0, 0, 255, 0));
+                Mat faceCapturada = new Mat(imagemCinza, dadosFace);
+                resize(faceCapturada, faceCapturada, new Size(160, 160));
+                if (teclado == null) {
+                    teclado = cFrame.waitKey(5);
+                }
+                if (teclado != null) {
+                    if (teclado.getKeyChar() == 'q') {
+                        if (countAmostra <= numeroAmostras) {
+                            imwrite("javacv/src/main/fotos/pessoa." + idPessoa + "." + countAmostra + ".jpg",
+                                    faceCapturada);
+                            System.out.println("Foto" + countAmostra + " capturada\n");
+                            countAmostra++;
+                        }
+                    }
+                    teclado = null;
+                }
+            }
+
+            if (teclado == null) {
+                teclado = cFrame.waitKey(20);
             }
             if (cFrame.isVisible()) {
                 cFrame.showImage(frameCapturado);
+            }
+
+            if (countAmostra > numeroAmostras) {
+                break;
             }
         }
         cFrame.dispose();
---
+++ src/main/java/com/facial_reconhecimento/Treinamento.java
@@ -0,0 +1,7 @@
+package com.facial_reconhecimento;
+
+public class Treinamento {
+    public static void main(String[] args) {
+        // Aqui eu preciso de mais 25 fotos de outra pessoa
+    }
+}
